a2195181d78d1e631e5b8428dfd454bd8874f553
alvaro2302 <alvaro230297@hotmail.com>
2022-06-10 05:07:59 -0400

    Initial Commit

---
+++ DemoAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoAPP
+//
+//  Created by Alvaro Cuiza on 10/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoAPP/DemoAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAPPApp.swift
+//  DemoAPP
+//
+//  Created by Alvaro Cuiza on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
