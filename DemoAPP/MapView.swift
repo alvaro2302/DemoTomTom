ce2ac1dc5b089ba616f1e248a5e10e9bc7ba3c63
alvaro2302 <alvaro230297@hotmail.com>
2022-06-10 05:25:49 -0400

    add proyect

--- DemoAPP/MapView.swift
+++ DemoAPP/MapView.swift
@@ -6,13 +6,21 @@
 //
 
 import SwiftUI
+import TomTomOnlineSDKMaps
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> TTMapView
+    {
+        let mapConfig = TTMapConfigurationBuilder.create().withMapKey("SoFs7GqvChoP0BDRzYiLhS3VoxMh6GQo").withTrafficKey("SoFs7GqvChoP0BDRzYiLhS3VoxMh6GQo").build()
+
+        return TTMapView(frame: CGRect.zero,mapConfiguration: mapConfig)
     }
-}
 
+   func updateUIView(_ uiView: TTMapView, context: UIViewRepresentableContext<MapView>)
+   {
+        // TBD: update map if something changes
+   }
+}
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
